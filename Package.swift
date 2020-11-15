83f6291890d88d0284b39d6cf310aae3004e1cab
Allan Poole <allan@allanpoole.com>
2020-11-16 09:30:50 +1000

    Fixed target reference.

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ConfigCat",
-            targets: ["ConfigCat iOS", "ConfigCat macOS", "ConfigCat tvOS", "ConfigCat watchOS"]),
+            targets: ["ConfigCat"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
