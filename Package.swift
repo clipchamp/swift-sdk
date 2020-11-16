ee9a586c40ace8be554ac2f0fd609435d204024b
Allan Poole <allan@allanpoole.com>
2020-11-16 11:20:00 +1000

    Fixed packages.

--- Package.swift
+++ Package.swift
@@ -5,14 +5,11 @@ import PackageDescription
 
 let package = Package(
     name: "ConfigCat",
-    platforms: [
-      .iOS(.v13)
-    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ConfigCat",
-            targets: ["ConfigCat XYZ"]),
+            targets: ["ConfigCat"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,16 +19,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "ConfigCat XYZ",
+            name: "ConfigCat",
             dependencies: ["ConfigCatVersion"],
             path: "Sources"),
         .target(
             name: "ConfigCatVersion",
             dependencies: [],
             path: "Version"),
-        .testTarget(
-            name: "ConfigCatTests",
-            dependencies: ["ConfigCatVersion", "ConfigCat XYZ"],
-            path: "Tests"),
     ]
 )
